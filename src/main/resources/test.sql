28576ad767d7037cda4b2861fc8947ef5c284712
Dmitriy Kanaev <kanaev.dm@mail.ru>
2020-02-01 12:51:50 +0300

    first commit

---
+++ src/main/resources/test.sql
@@ -0,0 +1,9 @@
+#insert into users (name, age, email) values ('Dima', 28, 'dima@mail.ru');
+#select * from users;
+#select * from users where id = 2;
+#select name, age from users where id = 1;
+#update users set name = 'Natalia', age = 31 where id = 2;
+#select name, age from users where name = 'Natalia';
+#delete from users;
+#delete from users where id = 4;
+#insert into users (name, age, email) values ('Sergey',63, 'sergey@google.com');
